f88ef8097ce87af5df5044b196a2e1790dc70073
Arthur Moreira da Silva <arthur.moreira@fbest.org.br>
2026-01-13 09:41:42 -0300

    aplicação pronta pra uso

---
+++ app.py
@@ -0,0 +1,142 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QMainWindow, QMessageBox
+from engine.mainwindow_qt import Ui_MainWindow
+from engine import fipe_client
+
+class FipeApp(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.buttonBrands.clicked.connect(self.listBrands)
+        self.buttonModels.clicked.connect(self.listModels)
+        self.buttonYears.clicked.connect(self.listYears)
+        self.buttonPrice.clicked.connect(self.listPrice)
+    
+    def list_att(self, title, items):
+        "Limpa e adiciona novos intems"
+        self.listWidget.clear()
+        self.listWidget.addItem(f" --- {title} --- ")
+        self.listWidget.addItems(items)
+    
+    def listBrands(self):
+        vehicle_type = self.comboBox.currentText()
+
+        if not vehicle_type:
+            QMessageBox.warning(self, "Atenção", "Escolha o tipo")
+            return
+
+        try:
+            self.listWidget.clear()
+            self.listWidget.addItem("Buscando tipos...")
+            QApplication.processEvents()
+
+            result = fipe_client.get_brands(vehicle_type)
+                
+                # nesse trecho realizo uma verificação : se o resultado é uma lista
+            if isinstance(result, list) and len(result) > 0:
+                list_format = [f"{i['name']} (Cod: {i['code']})" for i in result]
+                self.list_att("Marcas encontradas", list_format)
+            elif "error" in result:
+                self.list_att("ERRO", [result['message']])
+            else:
+                self.list_att('Aviso', ["Nenhum resultado."])
+        except Exception as e:
+            QMessageBox.critical(self, 'Erro', str(e))
+
+    def listModels(self):
+        vehicle_type = self.comboBox_2.currentText()
+        brand_id = self.codeBrands.text()
+
+        if not (vehicle_type and brand_id):
+            QMessageBox.warning(self, "Atenção", "Preencha os campos.")
+            return
+        
+        try:
+            self.listWidget.clear()
+            self.listWidget.addItem("Buscando modelos.")
+            QApplication.processEvents()
+
+            result = fipe_client.get_models(vehicle_type, int(brand_id))
+
+            if isinstance(result, list) and len(result) > 0:
+                list_format = [f"{i['name']} (Cod: {i['code']})" for i in result]
+                self.list_att("Modelos encontrados", list_format)
+            elif "error" in result:
+                self.list_att("ERRO", [result['message']])
+            else:
+                self.list_att('Aviso', ["Nenhum resultado."])
+        except Exception as e:
+            QMessageBox.critical(self, 'Erro', str(e))
+
+    def listYears(self):
+        vehicle_type = self.comboBox_3.currentText()
+
+        brand_id = self.codeBrands_2.text()
+        model_id = self.codeModels.text()
+
+        if not (brand_id and model_id):
+            QMessageBox.warning(self, "Atenção", "Preencha o codigo da Marca e do Modelo")
+            return
+        
+        try:
+            self.listWidget.clear()
+            self.listWidget.addItem("Buscando anos...")
+            QApplication.processEvents()
+
+            result = fipe_client.get_years(vehicle_type, int(brand_id), int(model_id))
+
+            if isinstance(result, list) and len(result) > 0:
+                list_format = [f'{i['name']} (Cód: {i['code']})' for i in result]
+                self.list_att("Anos encontrados", list_format)
+            elif "error" in result:
+                self.list_att("ERRO", [result['message']])
+            else:
+                self.list_att("Aviso", ["Nenhum resultado encontrado"])
+            
+        except Exception as e:
+            QMessageBox.critical(self, "Erro", str(e))
+    
+    def listPrice(self):
+        vehicle_type = self.comboBox_4.currentText()
+        brand_id = self.codeBrands_3.text()
+        model_id =  self.codeModels_2.text()
+        year_id = self.codeYears.text()
+
+        if not (brand_id and model_id and year_id):
+            QMessageBox.warning(self, "Atencção", "Preencha os campos.")
+            return
+        
+        try:
+            self.listWidget.clear()
+            self.listWidget.addItem("Buscando o preço.")
+            QApplication.processEvents()
+
+            data = fipe_client.get_price(vehicle_type, int(brand_id), int(model_id), year_id)
+
+            # pequeno teste de resultado
+            print("Retorno: ", data)
+
+            if "error" in data:
+                self.list_att("ERRO", [str(data)])
+            else:
+                details = [
+                    f"Veiculo: {data.get('model')}",
+                    f"Marca: {data.get('brand')}",
+                    f"Ano: {data.get('modelYear')}",
+                    f"Combustivel: {data.get('fuel')}",
+                    f"Código Fipe: {data.get('codeFipe')}",
+                    f"Mês Ref: {data.get('referenceMonth')}",
+                    f"-----------------------------------",
+                    f"PREÇO: {data.get('price')}"
+                ]
+                self.list_att('Preço Tabela Fipe', details)
+        except Exception as e:
+            QMessageBox.critical(self, "Erro", str(e))
+    
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = FipeApp()
+    window.show()
+    sys.exit(app.exec())
---
+++ engine/fipe_client.py
@@ -0,0 +1,76 @@
+"""
+================================================================================
+Aqui é o "cliente" que conversa com a API externa da FIPE.
+Sua única responsabilidade é fazer as requisições web (HTTP) para os
+endpoints corretos da API v2 e retornar os dados (ou um erro).
+
+Ele é o "motor" da nossa aplicação.
+================================================================================
+"""
+
+import httpx
+# biblioteca moderna para fazer requisições web ,usamos porque ela suporta `async`
+
+BASE_URL = "https://fipe.parallelum.com.br/api/v2"
+
+
+def get_brands(vehicle_type: str):
+    """Busca todas as marcas de um tipo de veículo"""
+    # Usando httpx.Client() em vez de AsyncClient()
+    with httpx.Client() as client:
+        try:
+            response = client.get(f"{BASE_URL}/{vehicle_type}/brands")
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            return {"error": f"Erro HTTP: {e.response.status_code}", "message": e.response.text}
+        except Exception as e:
+            return {"error": f"Ocorreu um erro: {e}"}
+
+def get_models(vehicle_type: str, brand_id: int):
+    """Busca os modelos de uma marca específica."""
+    with httpx.Client() as client:
+        try:
+            url = f"{BASE_URL}/{vehicle_type}/brands/{brand_id}/models"
+            response = client.get(url)
+            response.raise_for_status()
+
+            data = response.json()
+            
+            if isinstance(data, dict):
+                return data.get("models", [])
+            elif isinstance(data, list):
+                return data
+            else:
+                return []
+            
+        except httpx.HTTPStatusError as e:
+            return {"error": f"Erro HTTP: {e.response.status_code}", "message": e.response.text}
+        except Exception as e:
+            return {"error": f"Ocorreu um erro: {e}"}
+
+def get_years(vehicle_type: str, brand_id: int, model_id: int):
+    """Busca os anos de um modelo específico."""
+    with httpx.Client() as client:
+        try:
+            url = f"{BASE_URL}/{vehicle_type}/brands/{brand_id}/models/{model_id}/years"
+            response = client.get(url)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            return {"error": f"Erro HTTP: {e.response.status_code}", "message": e.response.text}
+        except Exception as e:
+            return {"error": f"Ocorreu um erro: {e}"}
+
+def get_price(vehicle_type: str, brand_id: int, model_id: int, year_id: str):
+    """Busca o preço final de um veículo específico."""
+    with httpx.Client() as client:
+        try:
+            url = f"{BASE_URL}/{vehicle_type}/brands/{brand_id}/models/{model_id}/years/{year_id}"
+            response = client.get(url)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            return {"error": f"Erro HTTP: {e.response.status_code}", "message": e.response.text}
+        except Exception as e:
+            return {"error": f"Ocorreu um erro: {e}"}
